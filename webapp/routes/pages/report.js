10d8117eac2cd58c36e30d7427b904d1711ba5ae
greg <gzussa@modeln.com>
2015-06-22 22:14:48 -0700

    - Fix issues around report creating. -- Create tmp folder automatically if it doesn't exist -- Fix duration bug

--- webapp/routes/pages/report.js
+++ webapp/routes/pages/report.js
@@ -4,6 +4,7 @@ var validator = require('validator');
 var excelbuilder = require('msexcel-builder-colorfix');
 var fs = require('fs');
 var batam_util = config = require('../../util/util.js');
+var mkdirp = require('mkdirp');
 
 /**
  * PAGE path /:build_id/report/:report_id
@@ -62,167 +63,167 @@ exports.download = function(req, res, next){
 				if(error){
 					return next(error);
 				}
-				
-				// Create a new workbook file in current working-path 
-				var workbook = excelbuilder.createWorkbook('./tmp/', report.name+'.xlsx');
-				var headerTextStyle = {bold: true};
-				var headerCellStyle = {bold: true};
-				var cellBorder = {left:'thin',top:'thin',right:'thin',bottom:'thin'};
-				//Populate the data array with tests found.
-				for(var i = 0; i < tests.length; i++){
-					// Create a new worksheet with 10 columns and 12 rows 
-					var stepsCount =  tests[i].steps != null? tests[i].steps.length : 0;
-					var sheet = workbook.createSheet(tests[i].name, 8, 8+stepsCount);
-					// Fill some data 
-					  sheet.set(1, 1, 'Test Name');
-					  sheet.font(1, 1, headerTextStyle);
-					  sheet.set(1, 2, 'Test Description');
-					  sheet.font(1, 2, headerTextStyle);
-					  sheet.set(1, 3, 'Duration');
-					  sheet.font(1, 3, headerTextStyle);
-					  sheet.set(1, 4, 'Tags');
-					  sheet.font(1, 4, headerTextStyle);
-					  
-					  sheet.set(2, 1, tests[i].name);
-					  sheet.set(2, 2, tests[i].description);
-					  if(tests[i].start_date != null && tests[i].end_date != null &&
-	  					_.isNumber(parseInt(tests[i].start_date)) && _.isNumber(parseInt(tests[i].end_date)) &&
-	  					_.isDate(new Date(parseInt(tests[i].start_date))) && _.isDate(new Date(tests[i].end_date)) &&
-	  					tests[i].start_date >= tests[i].end_date){
-						  sheet.set(2, 3, batam_util.durationToStr(tests[i].end_date - tests[i].start_date));
-					  }
-					  sheet.set(2, 4, tests[i].tags);
-					  
-					  if(tests[i].steps != null && tests[i].steps.length != 0){
-						  var inputVisibile = false;
-						  var expectedVisible = false;
-						  var statusVisible = false;
-						  var outputVisible = false;
-						  var durationVisible = false;
-						  var errorVisible = false;
-						  for (var j = 0; j < tests[i].steps.length; j++){
-							  if(tests[i].steps[j].input != null){
-								  inputVisibile = true;
-							  }
-							  if(tests[i].steps[j].expected != null){
-								  expectedVisible = true;
-							  }
-							  if(tests[i].steps[j].status != null){
-								  statusVisible = true;
-							  }
-							  if(tests[i].steps[j].output != null){
-								  outputVisible = true;
-							  }
-							  if(tests[i].steps[j].start_date != null && tests[i].steps[j].end_date != null){
-								  durationVisible = true;
-							  }
-							  if(tests[i].steps[j].error != null){
-								  errorVisible = true;
-							  }
-						  }
+				mkdirp('./tmp/', function(err) { 
+
+					// Create a new workbook file in current working-path 
+					var workbook = excelbuilder.createWorkbook('./tmp/', report.name+'.xlsx');
+					var headerTextStyle = {bold: true};
+					var headerCellStyle = {bold: true};
+					var cellBorder = {left:'thin',top:'thin',right:'thin',bottom:'thin'};
+					//Populate the data array with tests found.
+					for(var i = 0; i < tests.length; i++){
+						// Create a new worksheet with 10 columns and 12 rows 
+						var stepsCount =  tests[i].steps != null? tests[i].steps.length : 0;
+						var sheet = workbook.createSheet(tests[i].name, 8, 8+stepsCount);
+						// Fill some data 
+						  sheet.set(1, 1, 'Test Name');
+						  sheet.font(1, 1, headerTextStyle);
+						  sheet.set(1, 2, 'Test Description');
+						  sheet.font(1, 2, headerTextStyle);
+						  sheet.set(1, 3, 'Duration');
+						  sheet.font(1, 3, headerTextStyle);
+						  sheet.set(1, 4, 'Tags');
+						  sheet.font(1, 4, headerTextStyle);
 						  
-						  sheet.set(1, 6, 'Step #');
-						  sheet.border(1, 6, cellBorder);
-						  sheet.font(1, 6, headerTextStyle);
-						  sheet.set(2, 6, 'Name');
-						  sheet.border(2, 6, cellBorder);
-						  sheet.font(2, 6, headerTextStyle);
-						  var column = 3;
-						  if(inputVisibile){
-							  sheet.set(column, 6, 'Input Data');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
+						  sheet.set(2, 1, tests[i].name);
+						  sheet.set(2, 2, tests[i].description);
+						  if(tests[i].start_date != null && tests[i].end_date != null &&
+		  					_.isDate(new Date(tests[i].start_date)) && _.isDate(new Date(tests[i].end_date))){
+							  sheet.set(2, 3, batam_util.durationToStr(tests[i].end_date - tests[i].start_date));
 						  }
-						  if(expectedVisible){
-							  sheet.set(column, 6, 'Expected Result');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
-						  }
-						  if(statusVisible){
-							  sheet.set(column, 6, 'Status');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
-						  }
-						  if(outputVisible){
-							  sheet.set(column, 6, 'Output Data');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
-						  }
-						  if(durationVisible){
-							  sheet.set(column, 6, 'Execution Time');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
-						  }
-						  if(errorVisible){
-							  sheet.set(column, 6, 'Reason for Failure');
-							  sheet.border(column, 6, cellBorder);
-							  sheet.font(column, 6, headerTextStyle);
-							  column++;
-						  }
-						  for (var j = 0; j < tests[i].steps.length; j++){
-							  sheet.set(1, 7+j, tests[i].steps[j].order);
-							  sheet.border(1, 7+j, cellBorder);
-							  sheet.set(2, 7+j, tests[i].steps[j].name);
-							  sheet.border(2, 7+j, cellBorder);
+						  sheet.set(2, 4, tests[i].tags);
+						  
+						  if(tests[i].steps != null && tests[i].steps.length != 0){
+							  var inputVisibile = false;
+							  var expectedVisible = false;
+							  var statusVisible = false;
+							  var outputVisible = false;
+							  var durationVisible = false;
+							  var errorVisible = false;
+							  for (var j = 0; j < tests[i].steps.length; j++){
+								  if(tests[i].steps[j].input != null){
+									  inputVisibile = true;
+								  }
+								  if(tests[i].steps[j].expected != null){
+									  expectedVisible = true;
+								  }
+								  if(tests[i].steps[j].status != null){
+									  statusVisible = true;
+								  }
+								  if(tests[i].steps[j].output != null){
+									  outputVisible = true;
+								  }
+								  if(tests[i].steps[j].start_date != null && tests[i].steps[j].end_date != null){
+									  durationVisible = true;
+								  }
+								  if(tests[i].steps[j].error != null){
+									  errorVisible = true;
+								  }
+							  }
+							  
+							  sheet.set(1, 6, 'Step #');
+							  sheet.border(1, 6, cellBorder);
+							  sheet.font(1, 6, headerTextStyle);
+							  sheet.set(2, 6, 'Name');
+							  sheet.border(2, 6, cellBorder);
+							  sheet.font(2, 6, headerTextStyle);
 							  var column = 3;
 							  if(inputVisibile){
-								  sheet.set(column, 7+j, tests[i].steps[j].input);
-								  sheet.border(column, 7+j, cellBorder);
+								  sheet.set(column, 6, 'Input Data');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
 							  if(expectedVisible){
-								  sheet.set(column, 7+j, tests[i].steps[j].expected);
-								  sheet.border(column, 7+j, cellBorder);
+								  sheet.set(column, 6, 'Expected Result');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
 							  if(statusVisible){
-								  sheet.set(column, 7+j, tests[i].steps[j].status);
-								  sheet.border(column, 7+j, cellBorder);
+								  sheet.set(column, 6, 'Status');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
 							  if(outputVisible){
-								  sheet.set(column, 7+j, tests[i].steps[j].output);
-								  sheet.border(column, 7+j, cellBorder);
+								  sheet.set(column, 6, 'Output Data');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
 							  if(durationVisible){
-								  if(tests[i].steps[j].start_date != null && tests[i].steps[j].end_date != null &&
-				    					_.isNumber(parseInt(tests[i].steps[j].start_date)) && _.isNumber(parseInt(tests[i].steps[j].end_date)) &&
-				    					_.isDate(new Date(parseInt(tests[i].steps[j].start_date))) && _.isDate(new Date(tests[i].steps[j].end_date)) &&
-				    					tests[i].steps[j].start_date >= tests[i].steps[j].end_date){
-									  sheet.set(column, 7+j, batam_util.durationToStr(tests[i].steps[j].end_date - tests[i].steps[j].start_date));
-									  sheet.border(column, 7+j, cellBorder);
-								  }
+								  sheet.set(column, 6, 'Execution Time');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
 							  if(errorVisible){
-								  sheet.set(column, 7+j, tests[i].steps[j].error);
-								  sheet.border(column, 7+j, cellBorder);
+								  sheet.set(column, 6, 'Reason for Failure');
+								  sheet.border(column, 6, cellBorder);
+								  sheet.font(column, 6, headerTextStyle);
 								  column++;
 							  }
-						  }
-					}
-			    }  
-				
-				// Save it 
-				workbook.save(function(ok){
-				    if (!ok) {
-				      var file = fs.readFileSync('./tmp/'+report.name+'.xlsx', 'binary');
-				      res.setHeader('Content-disposition', 'attachment; filename='+report.name+'.xlsx');
-				      res.setHeader('Content-Length', file.length);
-				      res.write(file, 'binary');
-				      fs.unlinkSync('./tmp/'+report.name+'.xlsx');
-				      res.end();
-				    }else{
-				      workbook.cancel();
-				      res.end();
-				    }
-				  });
+							  for (var j = 0; j < tests[i].steps.length; j++){
+								  sheet.set(1, 7+j, tests[i].steps[j].order);
+								  sheet.border(1, 7+j, cellBorder);
+								  sheet.set(2, 7+j, tests[i].steps[j].name);
+								  sheet.border(2, 7+j, cellBorder);
+								  var column = 3;
+								  if(inputVisibile){
+									  sheet.set(column, 7+j, tests[i].steps[j].input);
+									  sheet.border(column, 7+j, cellBorder);
+									  column++;
+								  }
+								  if(expectedVisible){
+									  sheet.set(column, 7+j, tests[i].steps[j].expected);
+									  sheet.border(column, 7+j, cellBorder);
+									  column++;
+								  }
+								  if(statusVisible){
+									  sheet.set(column, 7+j, tests[i].steps[j].status);
+									  sheet.border(column, 7+j, cellBorder);
+									  column++;
+								  }
+								  if(outputVisible){
+									  sheet.set(column, 7+j, tests[i].steps[j].output);
+									  sheet.border(column, 7+j, cellBorder);
+									  column++;
+								  }
+								  if(durationVisible){
+									  if(tests[i].steps[j].start_date != null && tests[i].steps[j].end_date != null &&
+					    					_.isNumber(parseInt(tests[i].steps[j].start_date)) && _.isNumber(parseInt(tests[i].steps[j].end_date)) &&
+					    					_.isDate(new Date(parseInt(tests[i].steps[j].start_date))) && _.isDate(new Date(tests[i].steps[j].end_date)) &&
+					    					tests[i].steps[j].start_date >= tests[i].steps[j].end_date){
+										  sheet.set(column, 7+j, batam_util.durationToStr(tests[i].steps[j].end_date - tests[i].steps[j].start_date));
+										  sheet.border(column, 7+j, cellBorder);
+									  }
+									  column++;
+								  }
+								  if(errorVisible){
+									  sheet.set(column, 7+j, tests[i].steps[j].error);
+									  sheet.border(column, 7+j, cellBorder);
+									  column++;
+								  }
+							  }
+						}
+				    }  
+					
+					// Save it 
+					workbook.save(function(ok){
+					    if (!ok) {
+						      var file = fs.readFileSync('./tmp/'+report.name+'.xlsx', 'binary');
+						      res.setHeader('Content-disposition', 'attachment; filename='+report.name+'.xlsx');
+						      res.setHeader('Content-Length', file.length);
+						      res.write(file, 'binary');
+						      fs.unlinkSync('./tmp/'+report.name+'.xlsx');
+						      res.end();
+					    }else{
+						      workbook.cancel();
+						      res.end();
+					    }
+					  });
+				});
 			}
 				
 			//Handle Error.
@@ -243,7 +244,6 @@ exports.download = function(req, res, next){
 		//Create searchCriteria object.
 		var searchCriterias = batam_util.createSearchObject(req, criterias);
 		//Fetch tests based on defined searchCriterias
-		console.log(util.inspect(searchCriterias));
 		req.collections.tests.find(searchCriterias).toArray(findTests);
 	};
 
